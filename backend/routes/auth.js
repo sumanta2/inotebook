b191fa98ae8da39561e5d88bbe64054761318cff
sumanta2 <sumantagorai.21@gmail.com>
2021-10-30 11:12:52 +0530

    8th part vdo50ExtraPart

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const bcrypt= require('bcryptjs')
-const jwt=require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { body, validationResult } = require('express-validator')
 const User = require("../models/Users");
 const router = express.Router()
@@ -30,8 +30,8 @@ router.post('/createuser', [
             return res.status(400).json({ error: "Sorry a user Exist with this Email" })
         }
 
-        const salt= await bcrypt.genSalt(10)
-        const secPass= await bcrypt.hash(req.body.password,salt);    //the genSalt() and hash() method work like php md5 and sha1() method 
+        const salt = await bcrypt.genSalt(10)
+        const secPass = await bcrypt.hash(req.body.password, salt);    //the genSalt() and hash() method work like php md5 and sha1() method 
 
         //it insert the data in mongodb database 
         let myUser = await User.create({
@@ -42,21 +42,20 @@ router.post('/createuser', [
         })
 
         //token generation cade is here
-        const JWT_SECRET='Harryisagood$boy'
-        const data={
-            user:{              //here using jsonwebtoken package and its method we create a token(like php cookies) and send to the client
-                id:myUser.id
+        const JWT_SECRET = 'Harryisagood$boy'
+        const data = {
+            user: {              //here using jsonwebtoken package and its method we create a token(like php cookies) and send to the client
+                id: myUser.id
             }
         }
-        const jwtData=jwt.sign(data,JWT_SECRET)  //sign() method get data,secretKey string(2nd parameter and generate token)
-        res.json(jwtData)  
+        const jwtData = jwt.sign(data, JWT_SECRET)  //sign() method get data,secretKey string(2nd parameter and generate token)
+        res.json(jwtData)
     }
 
 
-    catch (err) 
-    {
+    catch (err) {
         console.error(err)  //it work same as console.log() when we pass exception object in side console.error() 
-        res.status(500).send("Sone Error Occur")
+        res.status(500).send("Internal Server Error")
     }
 
     // // console.log(req.body)
@@ -68,6 +67,54 @@ router.post('/createuser', [
     // res.send(req.body)
 })
 
+
+
+
+
+
+//Authenticate a User using: POST "/api/auth/login", NO login required
+router.post('/login', [
+
+    body('email', "Enter a valid Email<this is error message>").isEmail(),                   //with proper error message
+    body('password', "Enter a valid Password<this is error message>").exists()
+
+    //If there are errors, return Bad request and the errors
+], async (req, res) => {
+
+    //If there are errors, return Bad request and the errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: "errors.array()" })
+    }
+    const {email,password}=req.body; //here object destructuring concept is used
+
+    try{
+
+        let user=await User.findOne({email:email})  //findOne method fetch data form mongodb database according to email inputted by user
+        if(!user)
+        {
+            res.status(400).json({error:"Please try to login using correct Data"})
+        }
+
+        const passwordCompare= await bcrypt.compare(password,user.password)
+        if(!passwordCompare)
+        {
+            res.status(400).json({error:"Please try to login using correct Data"})
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const JWT_SECRET = 'Harryisagood$boy'
+        const authToken=jwt.sign(data,JWT_SECRET)   //here generate the token(like session in PHP)
+        res.json({authToken})
+    }
+    catch(error){
+        console.error(error)  //it work same as console.log() when we pass exception object in side console.error() 
+        res.status(500).send("Internal Server Error")
+    }
+})
 module.exports = router;
 
 
