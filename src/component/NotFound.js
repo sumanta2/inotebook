341db7318477730d33b2f87a2c4d1e72b0a86c5d
sumanta2 <sumantagorai.21@gmail.com>
2022-02-20 14:11:46 +0530

    36th change 404 page

--- src/component/NotFound.js
+++ src/component/NotFound.js
@@ -1,18 +1,23 @@
 import React,{useEffect} from 'react'
 import lottie from 'lottie-web'
 import Error404Animation from '../animation/84918-404-error-doodle-animation.json'
+import { useHistory } from 'react-router-dom';
 //import  '../../public/84918-404-error-doodle-animation.json'
 
 const NotFound=()=>{
-    useEffect(() => {
-        lottie.loadAnimation({
-          container: document.querySelector("#react_logo"),
-          animationData:Error404Animation,
-        });
-      }, []);
+  const history = useHistory()
+  history.push("/")
+    // useEffect(() => {
+    //     lottie.loadAnimation({
+    //       container: document.querySelector("#react_logo"),
+    //       animationData:Error404Animation,
+    //     });
+    //   }, []);
+
+      
     return(
     <div className=' d-flex flex-column justify-content-center align-items-center' style={{'backgroundColor':'rgb(153 233 255)','width':'80vw', 'height':'76vh'}}>
-        <h1>404 not found</h1>
+        <h1>404 not found Please go to home page</h1>
         <div id="react_logo" style={{'width':'100vw', 'height':'46vh'}}>
 
         </div>
